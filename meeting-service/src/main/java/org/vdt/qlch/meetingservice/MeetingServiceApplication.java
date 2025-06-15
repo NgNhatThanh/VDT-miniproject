d90495681fc27b8d64ae603b1062ffaf060c315d
NgNhatThanh <thanhnn.b22cn795@stu.ptit.edu.vn>
2025-06-15 22:30:20 +0700

    chore

--- meeting-service/src/main/java/org/vdt/qlch/meetingservice/MeetingServiceApplication.java
+++ meeting-service/src/main/java/org/vdt/qlch/meetingservice/MeetingServiceApplication.java
@@ -1,5 +1,7 @@
 package org.vdt.qlch.meetingservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,10 +13,17 @@ import org.vdt.qlch.meetingservice.config.ServiceUrlConfig;
 )
 @EnableConfigurationProperties({ServiceUrlConfig.class})
 @EnableCaching
-public class MeetingServiceApplication{
+public class MeetingServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MeetingServiceApplication.class, args);
     }
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String avc;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(avc);
+    }
 }
